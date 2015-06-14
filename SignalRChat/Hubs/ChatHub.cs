bcb7016ce3249a613f9dad3a4952fafbac8ee317
Richard Flores <richardrflores1@gmail.com>
2015-06-14 07:54:06 -0500

    Initial commit

---
+++ SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,10 @@
+﻿namespace SignalRChat.Hubs
+{
+    public class ChatHub : Microsoft.AspNet.SignalR.Hub
+    {
+        public void BroadcastMessage(string message)
+        {
+            Clients.All.postMessage(message);
+        }
+    }
+}
---
+++ SignalRChat/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Owin;
+
+namespace SignalRChat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // This is an interface to OWIN, a new standard interface between
+            // web servers and web applications. Eliminates dependency on IIS.
+            // SignalR uses this; allows for self hosting and other cool stuff.
+            appBuilder.MapSignalR();
+        } 
+    }
+}
